3d2cec67c4e82fcc431d2c366a938bf10eaad7e1
caffeines <me.caffeines@gmail.com>
2019-05-23 20:08:46 +0600

    Simple Linear Regression

--- Regression/Simple Linear Regression/Simple-Linear-Regression.py
+++ Regression/Simple Linear Regression/Simple-Linear-Regression.py
@@ -32,3 +32,11 @@ plt.title("Salary vs Experience (Training set)")
 plt.xlabel("Years of Experience");
 plt.ylabel("Salary")
 plt.show()
+
+# Visualising the test set result
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = "blue")
+plt.title("Salary vs Experience (Test set)")
+plt.xlabel("Years of Experience");
+plt.ylabel("Salary")
+plt.show()
