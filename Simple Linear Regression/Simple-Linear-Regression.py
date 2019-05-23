2b05049be833ba959a3b73e9874d7dc76a806ae6
caffeines <me.caffeines@gmail.com>
2019-05-23 19:45:53 +0600

    Simple Linear Regression

--- Simple Linear Regression/Simple-Linear-Regression.py
+++ Simple Linear Regression/Simple-Linear-Regression.py
@@ -16,3 +16,19 @@ y = dataset.iloc[:, 1].values
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 1/3, random_state = 0)
+
+# Fitting Simple Linear Regression to the training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the test set values
+y_pred = regressor.predict(X_test)
+
+# Visualising the training set result
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = "blue")
+plt.title("Salary vs Experience (Training set)")
+plt.xlabel("Years of Experience");
+plt.ylabel("Salary")
+plt.show()
