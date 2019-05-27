fdd1eb5a3b0af793d01b900e902e3a802f4c30ad
caffeines <me.caffeines@gmail.com>
2019-05-28 01:26:26 +0600

    regressor

--- Regression/Multiple Linear Regression/Multiple-Linear-Regression.py
+++ Regression/Multiple Linear Regression/Multiple-Linear-Regression.py
@@ -11,8 +11,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 dataset = pd.read_csv("50_Startups.csv")
-X = dataset.iloc[:, :-1]
-y = dataset.iloc[:, 4]
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
 
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+le_X = LabelEncoder()
+X[:, 3] = le_X.fit_transform(X[:, 3])
+ohe = OneHotEncoder(categorical_features=[3])
+X = ohe.fit_transform(X).toarray()
 
+# Avoiding the dummy variables trap
+X = X[:, 1:]
 
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, random_state = 42)
+
+# Fitting multiple linear regression to the training set
